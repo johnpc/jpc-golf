b6abb388370cae3e4166130b1ab1bf39a3a0e1cf
John Corser <xss@amazon.com>
2021-05-02 12:32:49 -0400

    feat(registration): gate new registrations at 8 players

--- src/App.js
+++ src/App.js
@@ -5,19 +5,26 @@ import RegistrationApp from "./RegistrationApp";
 import Amplify from "aws-amplify";
 import aws_exports from "./aws-exports";
 import {BrowserRouter as Router, Switch, Route, Link} from "react-router-dom";
-
 import React, {useEffect, useRef} from "react";
-
+import getPlayers from "./data/getPlayers";
 Amplify.configure(aws_exports);
+const LEAGUE_SIZE = 8;
 
 function App() {
   const appLink = useRef(null);
   const adminLink = useRef(null);
   const registrationLink = useRef(null);
   useEffect(() => {
-    if (window.location.pathname === "/") {
-      registrationLink.current.click();
-    }
+    const navigate = async () => {
+      const players = await getPlayers();
+      if (players.length >= LEAGUE_SIZE) {
+        appLink.current.click();
+      }
+      if (window.location.pathname === "/") {
+        registrationLink.current.click();
+      }
+    };
+    navigate();
   }, []);
 
   return (
