92db7a08700e973660fed3e031689160639b0474
John Corser <johnpc@umich.edu>
2021-06-09 17:20:44 -0400

    chore: update the about page

--- src/components/DisplayInformation.js
+++ src/components/DisplayInformation.js
@@ -1,61 +1,102 @@
 function DisplayInformation() {
   return (
     <div style={{padding: "1vw"}}>
-      <h1>Information about the League</h1>
-      <ul>
-        <li>League is hosted at Huron Hills Golf Course</li>
-        <li>
-          League price is $106 for 8 weeks of play. Cost includes $13 green fee.
-          An additional $7 cart fee may be paid if you'd like to use a cart.
-        </li>
-        <li>
-          Payment must be recieved in advance to complete your registration.
-          Easiest payment method is via <a href="https://venmo.com/">Venmo</a>{" "}
-          to @johnpc
-        </li>
-        <li>8 weeks beginning week of June 7</li>
-        <li>Tee times starting at 5:45pm each Thursday</li>
-        <li>9-hole rounds with cart</li>
-        <li>Play rotates between the back and front 9</li>
-        <li>Teams include two golfers</li>
-        <li>
-          Matches are 1 team vs 1 team, stroke play. Handicap adjusted low
-          handicap player on each team is 1 point. High handicap vs high
-          handicap player is another point. Total handicap-adjusted score for
-          the entire team is the third possible point you can score each week.
-        </li>
-        <li>Handicaps will be calculated after the first two weeks of play</li>
-        <li>
-          After handicaps are calculated, scoring will begin. Handicaps will
-          update each week after scores are submitted.
-        </li>
-        <li>
-          You are responsible for submitting your own scores. If you do not
-          report a score, you will lose your matchup.
-        </li>
-        <li>
-          If an odd number of teams register, you and your partner may play
-          alone. This round will not be counted toward your score.
-        </li>
-        <li>Gimmies for close putts can be offered by your opponent.</li>
-        <li>
-          The maximum score on any hole is double par. If you reach double par,
-          just pick up your ball, shake it off, move on, and report your score
-          as double par for the hole.
-        </li>
-        <li>
-          This league is relying on the honor system - please do not cheat.
-        </li>
-        <li>
-          There is no prize for winning the league except bragging rights, but
-          we encourage side bets among your playing partners.
-        </li>
-        <li>
-          Contact me at john@johncorser.com or text me at 989-598-7705. Note: I
-          have my phone set up to automatically silence unknown callers, so
-          until I have you in my contacts calling is not an option.
-        </li>
-      </ul>
+      <h1>Welcome to the 2021 GolfA2 League!</h1>
+      <p>
+        Rounds are every Thursday for 8 weeks, starting June 10th at Huron Hills
+        Golf Course. Please arrive around 5:30 for tee times starting at 5:40
+        and 5:50.
+      </p>
+      <p>
+        Your league fee of $110 includes only your greens fee - if you'd like to
+        rent a golf cart, you can work that out with the pro shop. I believe
+        cart rental is something like $8 a round.
+      </p>
+      <p>
+        After the round, I'll be going to Buffalo Wild Wings for their BOGO
+        wings deal, all are encouraged to join!
+      </p>
+      <h2>Rules</h2>
+      <p>
+        Pace of play is paramount - if you lose your ball in the rough, don't
+        spend more than 2 minutes looking for it. Take a free drop in the area
+        where it was last seen (only balls lost in hazards/out-of-bounds are
+        assessed penalty). If you're racking up strokes, pick up your ball when
+        you reach double par for the hole - that is the max score per hole.
+      </p>
+      <p>
+        You may move your golf ball a few inches with your club head to improve
+        your lie before swinging, but you may *not* move it to take an obstacle
+        out of play. "Swing-and-a-miss" shots are not scored, and if you graze a
+        ball to where you can still reach it with your golf club without moving
+        your feet, you can redo the shot without counting the stroke (this may
+        include chipping and sand shots as well).
+      </p>
+      <p>
+        A putt is automatically considered "good" if you can lay your putter
+        down to where part of the putter touches both the hole and the ball.
+      </p>
+      <p>Two mulligans are allowed per round, but cannot be used for putts.</p>
+      <p>
+        Only your first 9 holes are scored. If more holes are played after the
+        first 9, do not include them when reporting your score.
+      </p>
+      <h2>Reporting Your Score</h2>
+      <p>
+        Scores should be reported on golfa2.com. Note that you should only
+        report your score over par for the nine holes. So if you get a bogey on
+        every hole, you should report a "9".
+      </p>
+      <p>
+        The first two weeks of play are very important to calculate your initial
+        handicap, which will continue updating throughout the league.
+      </p>
+      <p>
+        Those raw scores are then converted into a league score. There are three
+        league score points possible each week. The high handicap players on
+        each team, the low handicap players on each team, then a combined "team"
+        point. These are automatically calculated after all scores are reported
+        on golfa2.com, so you don't have to worry about it too much.
+      </p>
+      <h2>Missing A Week</h2>
+      <p>
+        If you must miss a week, try to find an alternate in your place. There
+        have been a number of volunteers to be alternates in case you don't know
+        anyone yourself.
+      </p>
+      <p>
+        Try to make up your score by playing another round before the next
+        league day and report that score - otherwise you will forfeit both your
+        point and your team point.
+      </p>
+      <h2>Prizes!</h2>
+      <p>
+        Of course, bragging rights should be your primary motivation to win. But
+        to up the stakes, each first place winner will receive a $25 Amazon gift
+        card.
+      </p>
+      <p>
+        Additionally, the last place “winners” will be ordering Blazin’ wings at
+        our final post-league bdubs outing.
+      </p>
+      <p>
+        Don’t worry - scores are handicap adjusted so you’ll have a fair chance
+        of winning regardless of skill level.
+      </p>
+      <h2>Optional: Snake!</h2>
+      <p>
+        Each group will start the round with one "snake" token. Anytime someone
+        (participating) three putts (or worse), they must carry the snake token.
+      </p>
+      <p>
+        Whoever is left holding the snake token at the end of the round must buy
+        a drink for the other participants in their group.
+      </p>
+      <p>
+        When a golfer faces a long putt or has to drain their third, the other
+        golfers in the group may hissssss like a snake to get in that golfer's
+        head.
+      </p>
     </div>
   );
 }
